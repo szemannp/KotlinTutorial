873a7a653229dd25958436616e6a978bac86874f
Szemann Peter <pete@rollet.hu>
2019-06-05 22:26:57 -0400

    added vampire, override takeDamage of superclass (enemy)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,15 @@
 fun main() {
-    val enemy = Enemy("test enemy", 10, 3)
-
-    enemy.takeDamage(4)
-    println(enemy)
-
-    enemy.takeDamage(11)
-    println(enemy)
 
     val uglyTroll = Troll("Ugly Troll")
 
     println(uglyTroll)
 
-    uglyTroll.takeDamage(8)
-    println(uglyTroll)
-
     uglyTroll.takeDamage(30)
     println(uglyTroll)
 
+    val vlad = Vampire("Vlad")
+    println(vlad)
+
+    vlad.takeDamage(8)
+    println(vlad)
 }
---
+++ src/main/kotlin/Vampire.kt
@@ -0,0 +1,5 @@
+class Vampire(name: String) : Enemy(name, 20, 3) {
+    override fun takeDamage(damage: Int) {
+        super.takeDamage(damage / 2)
+    }
+}
